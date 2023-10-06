a0d502b647051f1d14787e6c16485e5ae9ed6975
HerbutAndrii <gerbuta91@gmail.com>
2023-10-06 20:57:53 +0300

    fixes

--- resources/views/list/layout.blade.php
+++ resources/views/list/layout.blade.php
@@ -4,11 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
-<<<<<<< HEAD
-    <link rel="stylesheet" href="/styles.css" />
-=======
     <link rel="stylesheet" href="/styles.css"/>
->>>>>>> 5baef61 (Add items)
 </head>
 <body>
 @yield('body')
