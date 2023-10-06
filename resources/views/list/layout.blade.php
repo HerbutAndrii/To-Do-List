75d078932dd86621e84fb0cda72ff6c2416abaed
HerbutAndrii <gerbuta91@gmail.com>
2023-10-06 19:49:56 +0300

    Add items

--- resources/views/list/layout.blade.php
+++ resources/views/list/layout.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title')</title>
+<<<<<<< HEAD
     <link rel="stylesheet" href="/styles.css" />
+=======
+    <link rel="stylesheet" href="/styles.css"/>
+>>>>>>> 5baef61 (Add items)
 </head>
 <body>
 @yield('body')
